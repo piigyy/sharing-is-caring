a8786e00da32409be628865266ee5e2e5e722ee3
Muhammad Ilham <muhd.iiilham@gmail.com>
2022-01-28 09:51:45 +0700

    feat: Product Color Schemas

--- frontend/src/views/Products.vue
+++ frontend/src/views/Products.vue
@@ -13,22 +13,18 @@
             <div class="card" style="width: 18rem;">
               <img src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_Green.png" class="card-img-top p-3 card-bg" alt="Spotify Logo">
               <div class="card-body card-bg">
-                <h5 class="card-title" style="color: #FF0000">Spotify Family</h5>
+                <h5 class="card-title product-title">Spotify Family</h5>
                 <section class="card-text">
                   <ul slot="color: black;">
                     <li>Host*: <span class="price">Rp 79,000/month</span></li>
-                    <li>Reg User:
-                      <ul>
-                        <li><span class="price">Rp 17,000/month</span></li>
-                        <li><span class="price">Rp 57,000/3 months</span></li>
-                      </ul>
-                    </li>
+                    <li>Reg User: <span class="price">Rp 17,000/month</span></li>
+                    <li>Reg User: <span class="price">Rp 57,000/3 months</span></li>
                   </ul>
                 </section>
               </div>
               <ul class="list-group list-group-flush">
-                <li class="list-group-item option card-bg"><router-link class="nav-link card-bg" to="/host/spotify">Become a host</router-link></li>
-                <li class="list-group-item option card-bg"><router-link class="nav-link card-bg" to="/buy/spotify">Order</router-link></li>
+                <li class="list-group-item option card-bg"><router-link class="nav-link card-bg" to="/host/spotify"><span class="action-order">Become a host</span></router-link></li>
+                <li class="list-group-item option card-bg"><router-link class="nav-link card-bg" to="/buy/spotify"><span class="action-order">Order</span></router-link></li>
               </ul>
             </div>
           </div>
@@ -36,7 +32,7 @@
             <div class="card" style="width: 18rem;">
               <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png" class="card-img-top p-3 card-bg" alt="Netflix Logo">
               <div class="card-body card-bg">
-                <h5 class="card-title" style="color: #FF0000">Netflix</h5>
+                <h5 class="card-title product-title">Netflix</h5>
                 <section class="card-text">
                   <ul>
                     <li>Host*: <span class="price">Rp 188,000/month</span></li>
@@ -45,11 +41,28 @@
                 </section>
               </div>
               <ul class="list-group list-group-flush">
-                <li class="list-group-item option card-bg"><router-link class="nav-link card-bg" to="/host/netflix">Become a host</router-link></li>
-                <li class="list-group-item option card-bg"><router-link class="nav-link card-bg" to="/buy/netflix">Order</router-link></li>
+                <li class="list-group-item option card-bg"><router-link class="nav-link card-bg" to="/host/netflix"><span class="action-order">Become a host</span></router-link></li>
+                <li class="list-group-item option card-bg"><router-link class="nav-link card-bg" to="/buy/netflix"><span class="action-order">Order</span></router-link></li>
               </ul>
             </div>
           </div>
+          <!-- <div class="col">
+            <div class="card" style="width: 18rem;">
+              <img src="https://logos-world.net/wp-content/uploads/2020/04/Twitter-Logo-2010-2012.png" class="card-img-top p-3 card-bg" alt="Twitter Logo">
+              <div class="card-body card-bg">
+                <h5 class="card-title product-title">Twitter Private Account</h5>
+                <section class="card-text">
+                  <ul>
+                    <li><i>based on host offers</i></li>
+                  </ul>
+                </section>
+              </div>
+              <ul class="list-group list-group-flush">
+                <li class="list-group-item option card-bg"><router-link class="nav-link card-bg" to="/host/twitter"><span class="action-order">Become a host</span></router-link></li>
+                <li class="list-group-item option card-bg"><router-link class="nav-link card-bg" to="/buy/twitter"><span class="action-order">Order</span></router-link></li>
+              </ul>
+            </div>
+          </div> -->
         </div>
       </div>
     </div>
@@ -79,5 +92,14 @@ export default {
   
   .card-bg {
     background-color: #750550;
+  }
+
+  .product-title {
+    color: snow;
+  }
+
+  .action-order {
+    color: wheat;
+    font-weight: bold;
   }
 </style>
