e1b20cd1288a9413283d3c6836a79b4e0484aec7
Muhammad Ilham <muhd.iiilham@gmail.com>
2022-02-04 20:16:20 +0700

    feat: Add Logging When Reading Config

--- backend/config/config.go
+++ backend/config/config.go
@@ -3,6 +3,7 @@ package config
 import (
 	"flag"
 	"fmt"
+	"log"
 
 	"github.com/spf13/viper"
 )
@@ -14,6 +15,7 @@ func ReadConfig() (*Config, error) {
 	flag.Parse()
 
 	configFileName := fmt.Sprintf("config.%s.yaml", *mode)
+	log.Printf("reading config: %s\n", configFileName)
 
 	viper.SetConfigType("yaml")
 	viper.AddConfigPath("../")
@@ -21,15 +23,18 @@ func ReadConfig() (*Config, error) {
 
 	readCfgErr := viper.ReadInConfig()
 	if readCfgErr != nil {
+		log.Printf("error viper.ReadInConfig: %v\n", readCfgErr)
 		return nil, readCfgErr
 	}
 
 	unmarshallErr := viper.Unmarshal(&cfg)
 
 	if unmarshallErr != nil {
+		log.Printf("error viper.Unmarshal: %v\n", readCfgErr)
 		return nil, unmarshallErr
 	}
 
+	log.Printf("success reading config: %s\n", configFileName)
 	return &cfg, nil
 
 }
