a6606bc9246ea38033c1faba8b4280044706096b
Muhammad Ilham <muhd.iiilham@gmail.com>
2022-02-18 09:36:27 +0700

    feat: Package Logger

---
+++ backend/pkg/logger/log.go
@@ -0,0 +1,63 @@
+package logger
+
+import (
+	"context"
+
+	"github.com/sirupsen/logrus"
+)
+
+type RequestID string
+
+var RequestIDKey RequestID = "request-id"
+
+func Info(ctx context.Context, caller, format string, values ...interface{}) {
+	requestID := "-"
+	ctxVal := ctx.Value(RequestIDKey)
+	if ctxVal != nil {
+		requestID = ctxVal.(string)
+	}
+
+	logrus.WithFields(logrus.Fields{
+		"caller":     caller,
+		"request-id": requestID,
+	}).Infof(format, values...)
+}
+
+func Error(ctx context.Context, caller, format string, values ...interface{}) {
+	requestID := "-"
+	ctxVal := ctx.Value(RequestIDKey)
+	if ctxVal != nil {
+		requestID = ctxVal.(string)
+	}
+
+	logrus.WithFields(logrus.Fields{
+		"caller":     caller,
+		"request-id": requestID,
+	}).Errorf(format, values...)
+}
+
+func Warn(ctx context.Context, caller, format string, values ...interface{}) {
+	requestID := "-"
+	ctxVal := ctx.Value(RequestIDKey)
+	if ctxVal != nil {
+		requestID = ctxVal.(string)
+	}
+
+	logrus.WithFields(logrus.Fields{
+		"caller":     caller,
+		"request-id": requestID,
+	}).Warnf(format, values...)
+}
+
+func Fatal(ctx context.Context, caller, format string, values ...interface{}) {
+	requestID := "-"
+	ctxVal := ctx.Value(RequestIDKey)
+	if ctxVal != nil {
+		requestID = ctxVal.(string)
+	}
+
+	logrus.WithFields(logrus.Fields{
+		"caller":     caller,
+		"request-id": requestID,
+	}).Fatalf(format, values...)
+}
